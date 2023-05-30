455c08d3852af8ce3670da31cb4d9ef15e62b6ad
RaulThunig <ersguterraul@gmail.com>
2023-05-30 10:14:42 +0200

    test

--- fittify/src/routes/+page.svelte
+++ fittify/src/routes/+page.svelte
@@ -7,7 +7,7 @@
 	<WelcomeNavigator />
 </Header>
 
-<div class="notification">
+<div class="notification is-dark">
     <h1>Welcome to Fittify app!</h1>
     <p>We still working on this page</p>
 </div>
